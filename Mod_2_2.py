a7ec279ceb79de5209777b005366929d7dc41af0
igornikolaev143 <igornikolaev143@gmail.com>
2024-08-20 17:53:41 +0400

    ooo

--- Mod_2_2.py
+++ Mod_2_2.py
@@ -1,7 +1,7 @@
 first = int(input(''))
 second = int(input(''))
 third = int(input(''))
-print
+print(first, second, third)
 if first == second == third:
     print ('3')
 elif first == second or second == third or first == third:
